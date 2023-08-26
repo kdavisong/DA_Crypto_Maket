24b55377c3f9010db0de879d70d995b1241246c6
kdavisong <kdavisongalvis@gmail.com>
2023-08-25 19:58:52 -0500

    versión 1.1.1

--- BD_sql/Consulta.sql
+++ BD_sql/Consulta.sql
@@ -1,19 +1,36 @@
-CREATE TABLE `ETH` (
+CREATE TABLE `ADA` (
 	`Date` DATE NULL,
-	`Year` DATE NULL,
+	`Year` YEAR NULL,
 	`Prices` DECIMAL(15,3) NULL DEFAULT NULL,
 	`Market_Caps` DECIMAL(15,3) NULL DEFAULT NULL,
 	`Total_Volumes` DECIMAL(15,3) NULL DEFAULT NULL,
 	`v_porcentual_d` DECIMAL(5,3) NULL DEFAULT NULL,
+	`MM65` DECIMAL(15,3) NULL DEFAULT NULL
 )
 COLLATE='latin1_swedish_ci'
 ;
 /* El nodo de tabla "BTC" no se encontró en el árbol. */
 
 
-LOAD DATA INFILE "PEPE.csv"
-INTO TABLE PEPE
+LOAD DATA INFILE "LDO.csv"
+INTO TABLE LDO
 FIELDS TERMINATED BY ',' ENCLOSED BY ''
 LINES TERMINATED BY '\n' IGNORE 1
-LINES(DATE,Year, Prices, Market_Caps, Total_Volumes, v_porcentual_d)
+LINES(Date,Year, Prices, Market_Caps, Total_Volumes, v_porcentual_d,MM65);
 
+CREATE TABLE `Inversiones` (
+	`ID` INT,
+	`Fecha` DATE,
+	`Criptomoneda` VARCHAR(5),
+	`Monto` DECIMAL(15,3) NULL DEFAULT NULL
+)
+COLLATE='latin1_swedish_ci'
+;
+
+LOAD DATA INFILE "Inversiones.csv"
+INTO TABLE Inversiones
+FIELDS TERMINATED BY ',' ENCLOSED BY ''
+LINES TERMINATED BY '\n' IGNORE 1
+LINES(ID,Fecha, Criptomoneda, Monto);
+
+DROP TABLE `ETH`;
